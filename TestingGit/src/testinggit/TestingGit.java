94d029b7c077ccc33a2d683a46404c5dfe8ed601
subas khanal <subas khanal>
2016-09-06 22:46:02 +0545

    First commit- java

---
+++ TestingGit/src/testinggit/TestingGit.java
@@ -0,0 +1,19 @@
+package testinggit;
+
+/**
+ *
+ * @author subas khanal
+ */
+public class TestingGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
